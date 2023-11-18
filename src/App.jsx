1b8c17b3827188c2068ad03a00217fcae96cdf6c
Giancarlos1024 <giancarlosvlsqz@gmail.com>
2023-11-17 23:27:05 -0500

    Se modificaron los archivos

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+import { Nosotros } from "./pages/Nosotros"
+
+
 function App() {
 
   return (
     <>
       <h1>Hola Mundo</h1>
+      <Nosotros></Nosotros>
     </>
   )
 }
---
+++ src/pages/Nosotros.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Nosotros = ({children}) => {
+  return (
+    <>
+        <h1>Nosotros</h1>
+        
+    </>
+  )
+}
