16687bd498b8478d9f61306ee2b9962d8c2166cf
Santiago <juanusca45@gmail.com>
2023-11-23 11:13:36 -0500

    update home

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import Slider from "@/components/slider/slider";
+import React, { Suspense, useEffect, useState } from "react";
 
 const Home = () => {
+  const [slides, setSlides] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:3001/slides")
+      .then((response) => response.json())
+      .then((data) => setSlides(data))
+      .catch((error) => console.error("Error:", error));
+  }, []);
   return (
-    <section className=' w-full bg-yellow h-64'>
-
+    <section className=" w-full h-80">
+      <Slider slides={slides} />
     </section>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
