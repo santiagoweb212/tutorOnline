28a26064799b389460970f4e226ea4971ff5f2b4
Santiago <juanusca45@gmail.com>
2023-11-23 11:12:04 -0500

    update css

--- src/components/modalCartShop.jsx
+++ src/components/modalCartShop.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const ModalCartShop = ({openModalCartShop}) => {
     
   return (
-    <div className="absolute w-60 bg-slate-200 right-0 " >
+    <div className="absolute w-60 bg-slate-200 right-0 z-20" >
       {openModalCartShop && (
           <article className="flex bg-red-500  items-center p-2">
           <img src="" alt="imgProduct" className="w-[5rem] h-16 bg-orange-200" />
