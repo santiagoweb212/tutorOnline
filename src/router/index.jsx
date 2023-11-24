ba9ae6fbc673777c9d87896e71e1a0ae469c31eb
Giancarlos1024 <giancarlosvlsqz@gmail.com>
2023-11-23 20:41:56 -0500

    se actualizo el archivo index.jsx

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,12 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
 /* import LayoutPublic from "@/layout/LayoutPublic"; */
 import LayoutPublic from "@/layout/layoutPublic";
+import { Nosotros } from '@/pages/Nosotros'
 const router = createBrowserRouter([{
     path: '/', element: <LayoutPublic />, children: [
         { index: true, element: <div>home</div> },
         { path: '/inicio-session', element: <div>login</div> },
         { path: '/registro', element: <div>register</div> },
-        { path: "/nosotros", element: <div>nosotros</div> },
+        { path: "/nosotros", element: <Nosotros /> },
     ]
 }]);
 export default router
