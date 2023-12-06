c53f06e9ac77fc9ace84381e31c1c25449004f30
Giancarlos1024 <giancarlosvlsqz@gmail.com>
2023-12-05 22:23:12 -0500

    se modificaron archivos

--- src/pages/Contact/components/formulario.jsx
+++ src/pages/Contact/components/formulario.jsx
@@ -1,94 +1,88 @@
+// En tu archivo formulario.jsx
 import React, { useState } from 'react';
 
 export const Formulario = () => {
-  const [formulario, setFormulario] = useState({
+  const [formData, setFormData] = useState({
     nombre: '',
     correo: '',
-    asunto: '',
     mensaje: '',
     archivo: null,
   });
 
-  const [errores, setErrores] = useState({
+  const [errors, setErrors] = useState({
     nombre: '',
     correo: '',
-    asunto: '',
     mensaje: '',
+    archivo: '',
+    general: '',
   });
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-
-    // Validar que en el campo de nombre solo se ingresen letras
-    if (name === 'nombre' && !/^[A-Za-z]+$/.test(value)) {
-      setErrores({
-        ...errores,
-        [name]: 'El nombre solo debe contener letras',
-      });
+  const handleInputChange = (e) => {
+    const { id, value } = e.target;
+  
+    // Validar que solo se ingresen letras
+    if (id === 'nombre' && !/^[A-Za-z]+$/.test(value)) {
+      setErrors({ ...errors, [id]: 'El nombre solo debe contener letras' });
     } else {
-      setErrores({
-        ...errores,
-        [name]: '',
-      });
+      setErrors({ ...errors, [id]: '' });
     }
-
-    setFormulario({
-      ...formulario,
-      [name]: value,
-    });
+  
+    setFormData({ ...formData, [id]: value });
   };
 
-  const handleArchivoChange = (e) => {
-    const archivo = e.target.files[0];
-    setFormulario({
-      ...formulario,
-      archivo: archivo,
-    });
+  const handleFileChange = (e) => {
+    const { id, files } = e.target;
+    setFormData({ ...formData, [id]: files[0] });
   };
 
-  const validarFormulario = () => {
-    let erroresFormulario = {};
-
-    if (!formulario.nombre.trim()) {
-      erroresFormulario.nombre = 'El nombre es obligatorio';
-    }
-
-    if (!formulario.correo.trim()) {
-      erroresFormulario.correo = 'El correo electrónico es obligatorio';
-    } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(formulario.correo)) {
-      erroresFormulario.correo = 'El correo electrónico no es válido';
-    }
-
-    if (!formulario.asunto.trim()) {
-      erroresFormulario.asunto = 'El asunto es obligatorio';
-    }
-
-    if (!formulario.mensaje.trim()) {
-      erroresFormulario.mensaje = 'El mensaje es obligatorio';
-    }
-
-    setErrores(erroresFormulario);
-
-    // Si no hay errores, retorna true; de lo contrario, retorna false
-    return Object.keys(erroresFormulario).length === 0;
-  };
-
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
-    if (validarFormulario()) {
-      // Aquí puedes agregar la lógica para enviar el formulario, por ejemplo, una llamada a la API.
-      console.log('Formulario enviado:', formulario);
-      // Reiniciar el estado del formulario después de enviarlo si es necesario
-      setFormulario({
+    // Realizar validaciones aquí
+    const newErrors = {};
+    if (!formData.nombre.trim()) {
+      newErrors.nombre = 'El nombre es obligatorio';
+    }
+    // Agrega más validaciones según tus requisitos
+
+    if (Object.keys(newErrors).length > 0) {
+      setErrors(newErrors);
+      return;
+    }
+
+    // Aquí puedes realizar la lógica de envío del formulario
+    try {
+      const formDataToSend = new FormData();
+      formDataToSend.append('nombre', formData.nombre);
+      formDataToSend.append('correo', formData.correo);
+      formDataToSend.append('mensaje', formData.mensaje);
+      formDataToSend.append('archivo', formData.archivo);
+
+      await fetch('http://localhost:3001/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+
+      console.log('Formulario enviado:', formData);
+      setFormData({
         nombre: '',
         correo: '',
-        asunto: '',
         mensaje: '',
         archivo: null,
       });
-    } else {
-      console.log('Formulario no válido. Por favor, corrige los errores.');
+      setErrors({
+        nombre: '',
+        correo: '',
+        mensaje: '',
+        archivo: '',
+        general: '',
+      });
+    } catch (error) {
+      console.error('Error al enviar el formulario:', error);
+      setErrors({ ...errors, general: 'Error al enviar el formulario' });
     }
   };
 
@@ -99,20 +93,19 @@ export const Formulario = () => {
           <h1 className='text-4xl font-bold'>Formulario de Contacto</h1>
         </div>
         <div>
-          <form onSubmit={handleSubmit} className='italic'>
+          <form onSubmit={handleSubmit} className='italic' >
             <div>
               <label htmlFor='nombre'>Nombre:</label>
               <input
                 className='p-1 rounded border-2 border-gray-300'
                 type='text'
                 id='nombre'
-                name='nombre'
-                value={formulario.nombre}
-                onChange={handleChange}
-                style={{ width: '100%' }} // Ancho al 100%
+                value={formData.nombre}
+                onChange={handleInputChange}
+                style={{ width: '100%' }}
                 required
               />
-              <div className='error'>{errores.nombre}</div>
+              <div className='error'>{errors.nombre}</div>
             </div>
 
             <div>
@@ -121,41 +114,40 @@ export const Formulario = () => {
                 className='p-1 rounded border-2 border-gray-300'
                 type='email'
                 id='correo'
-                name='correo'
-                value={formulario.correo}
-                onChange={handleChange}
-                style={{ width: '100%' }} // Ancho al 100%
+                value={formData.correo}
+                onChange={handleInputChange}
+                style={{ width: '100%' }}
                 required
               />
-              <div className='error'>{errores.correo}</div>
+              <div className='error'>{errors.correo}</div>
             </div>
-
 
             <div>
               <label htmlFor='mensaje'>Mensaje:</label>
               <textarea
                 className='p-1 rounded border-2 border-gray-300'
                 id='mensaje'
-                name='mensaje'
-                value={formulario.mensaje}
-                onChange={handleChange}
-                style={{ width: '100%', height: '150px' }} // Ancho al 100% y altura de 150px
+                value={formData.mensaje}
+                onChange={handleInputChange}
+                style={{ width: '100%', height: '150px' }}
                 required
               ></textarea>
-              <div className='error'>{errores.mensaje}</div>
+              <div className='error'>{errors.mensaje}</div>
             </div>
 
             <div>
-              <label htmlFor='archivo'>Adjuntar Archivo :</label>
+              <label htmlFor='archivo'>Adjuntar Archivo:</label>
               <input
                 className='p-1 rounded'
                 type='file'
                 id='archivo'
-                name='archivo'
-                onChange={handleArchivoChange}
-                
+                onChange={handleFileChange}
+
               />
+              <div className='error'>{errors.archivo}</div>
             </div>
+
+            <div className='error'>{errors.general}</div>
 
             <div className='bg-black w-1/6 text-white text-center p-2 hover:bg-blue-500 mt-4 rounded'>
               <button type='submit'>Enviar</button>
